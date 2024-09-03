d5b600902716083734acfe06db2995801f5f389a
celer <cedric.leroy@smile.fr>
2024-09-03 12:56:00 +0200

    wip

--- vocab_dictionnary/dictionary/serializers/DictionarySerializer.py
+++ vocab_dictionnary/dictionary/serializers/DictionarySerializer.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from ..models import Dictionnary, Languages, Entry
+from ..models import Dictionnary
+
 
 class DictionarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Dictionnary
         fields = ['name', 'source_language', 'target_language']
+
+    def validate_data(self):
+        pass
