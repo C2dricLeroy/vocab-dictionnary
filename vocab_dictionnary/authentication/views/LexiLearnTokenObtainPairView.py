73c72985152f1fa5d1e54818958b665817ca220c
celer <cedric.leroy@smile.fr>
2024-10-24 18:27:04 +0200

    [IMP] set access & refresh tokens in cookies

--- vocab_dictionnary/authentication/views/LexiLearnTokenObtainPairView.py
+++ vocab_dictionnary/authentication/views/LexiLearnTokenObtainPairView.py
@@ -1,6 +1,38 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from ..serializers import (LexiLearnTokenObtainPairSerializer)
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from datetime import timedelta
 
 
 class LexiLearnTokenObtainPairView(TokenObtainPairView):
     serializer_class = LexiLearnTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        tokens = serializer.validated_data
+
+        response = Response(status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key='access_token',
+            value=tokens['access'],
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+            expires=timezone.now() + timedelta(days=1)
+        )
+
+        response.set_cookie(
+            key='refresh_token',
+            value=tokens['refresh'],
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+            expires=timezone.now() + timedelta(days=7)
+        )
+
+        return response
