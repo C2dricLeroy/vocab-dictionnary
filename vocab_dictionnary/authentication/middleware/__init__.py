e6070ed8dd6526cf07b3b5760f5c813ee3289529
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2024-11-11 07:28:48 +0100

    add a middleware to get httponly cookies from request

---
+++ vocab_dictionnary/authentication/middleware/__init__.py
@@ -0,0 +1,1 @@
+from .tokenAuthMiddleware import TokenAuthMiddleware
---
+++ vocab_dictionnary/authentication/middleware/tokenAuthMiddleware.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+class TokenAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        access_token = request.COOKIES.get('access_token')
+        if access_token:
+            request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+        return self.get_response(request)
